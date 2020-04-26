12fbeda3171be7699f26594e330ceefcece8a3dc
Praseetha-KR <praseetha04@gmail.com>
2020-04-26 19:09:36 +0530

    New command: from with flag: characters

--- main.go
+++ main.go
@@ -224,6 +224,36 @@ func main() {
 					return nil
 				},
 			},
+			{
+				Name:    "from",
+				Aliases: []string{"f"},
+				Usage:   "generate password from given characters",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:    "length",
+						Aliases: []string{"l"},
+						Value:   10,
+						Usage:   "length",
+					},
+					&cli.StringFlag{
+						Name:    "characters",
+						Aliases: []string{"c"},
+						Value:   "",
+						Usage:   "character set",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					l := validatedLength(c.Int("length"))
+					chars := c.String("characters")
+
+					r, err := strand.From(chars, l)
+					if err != nil {
+						return err
+					}
+					fmt.Println(r)
+					return nil
+				},
+			},
 		},
 	}
 
