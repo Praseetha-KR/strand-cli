a75ea1549828becc2b164e43caf73e80cb1b3359
Praseetha-KR <praseetha04@gmail.com>
2020-04-26 18:52:28 +0530

    New command: password with flags: simple & urlsafe

--- main.go
+++ main.go
@@ -175,6 +175,41 @@ func main() {
 					return nil
 				},
 			},
+			{
+				Name:    "password",
+				Aliases: []string{"p"},
+				Usage:   "generate password of random length",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:    "simple",
+						Aliases: []string{"s"},
+						Value:   false,
+						Usage:   "8 character password",
+					},
+					&cli.BoolFlag{
+						Name:    "urlsafe",
+						Aliases: []string{"u"},
+						Value:   false,
+						Usage:   "is urlsafe",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					isSimple := c.Bool("simple")
+					isURLSafe := c.Bool("urlsafe")
+					r := ""
+					if isURLSafe && isSimple {
+						r = strand.URLSafeSimplePassword()
+					} else if isURLSafe && !isSimple {
+						r = strand.URLSafePassword()
+					} else if !isURLSafe && isSimple {
+						r = strand.SimplePassword()
+					} else {
+						r = strand.Password()
+					}
+					fmt.Println(r)
+					return nil
+				},
+			},
 		},
 	}
 
