62b2753fe85a122ddf1c0c0a8758b895f082f0fc
Praseetha-KR <praseetha04@gmail.com>
2020-04-26 18:17:41 +0530

    New command: string

--- main.go
+++ main.go
@@ -2,10 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/Praseetha-KR/strand"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println("hello ", strand.Password())
+	app := &cli.App{
+		Name:  "strand",
+		Usage: "Random String Generator",
+		Commands: []*cli.Command{
+			{
+				Name:    "string",
+				Aliases: []string{"s"},
+				Usage:   "generate random string",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:    "length",
+						Aliases: []string{"l"},
+						Value:   10,
+						Usage:   "length",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					l := validatedLength(c.Int("length"))
+					r := strand.String(l)
+					fmt.Println(r)
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ validators.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+)
+
+func validatedLength(l int) int {
+	if l <= 0 {
+		log.Fatal("Invalid value for length flag: must be greater than 0")
+	}
+	return l
+}
